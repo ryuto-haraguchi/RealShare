25cb02a889ad76bcdbb4ab04e21f00828f127862
Phone Haraguchi’s <hr48691226@gmail.com>
2024-12-02 15:02:44 +0900

    enumの追加/日本語ファイルの追加

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,2 +1,10 @@
 class Notice < ApplicationRecord
+
+  enum status: { "unsupported": 0, "supported": 1 }
+
+  belongs_to :user
+  belongs_to :noticeable, polymorphic: true
+
+  validates :reason, presence: true
+  
 end
