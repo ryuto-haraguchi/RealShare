82aedb0f22d547c5fc05f4a68fc1a7baf98adddd
Phone Haraguchi’s <hr48691226@gmail.com>
2024-12-02 23:42:35 +0900

    ユーザー登録画面まで

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_directory ../stylesheets/public .css
+//= link_directory ../stylesheets/public/homes .css
+//= link_directory ../stylesheets/public/registrations .css
