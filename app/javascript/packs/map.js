6e287fa923b5e85aa433180513611d4d2b5585fd
EC2 Default User <ec2-user@ip-172-31-39-174.eu-north-1.compute.internal>
2024-12-12 16:43:31 +0000

    追加調整

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -43,15 +43,14 @@ async function initMap() {
         position: { lat: latitude, lng: longitude },
         map,
         title: town,
+        maxWidth: 400,
       });
 
       // 投稿一覧を情報ウィンドウに表示
       const contentString = `
-        <div class="container mt-3">
-          <div class="row d-flex justify-content-center">
+          <div class="d-flex justify-content-center">
             <h5>${town}の投稿一覧</h5>
           </div>
-        </div>
         ${posts
           .map((post) => {
           const postPath = post.postPath;
@@ -59,25 +58,19 @@ async function initMap() {
           const content = post.content;
           const fullAddress = post.fullAddress;
           const createdAt = post.createdAt;
-            return `
-            <div class="container mt-3">
-              <div class="row">
-              <div class="col">
-                <div class="card mb-4">
-                <div class="card-header">
-                  <strong class="ml-3"><a href="${postPath}" class="text-dark">${title}</a></strong>
-                </div>
-                <div class="card-body">
-                  <p class="card-text">${content}</p>
-                </div>
-                <div class="card-footer text-muted d-flex justify-content-between">
-                  <span>${createdAt}</span>
-                  <span>${fullAddress}</span>
-                </div>
-                </div>
-              </div>
-              </div>
+          return `
+          <div class="card mb-2">
+            <div class="card-header">
+              <strong class="ml-3"><a href="${postPath}" class="text-dark">${title}</a></strong>
             </div>
+            <div class="card-body">
+              <p class="card-text"><strong>${content}</strong></p>
+            </div>
+            <div class="card-footer text-muted d-flex justify-content-between">
+              <span class="mr-5">${createdAt}</span>
+              <span class="ml-5">${fullAddress}</span>
+            </div>
+          </div>
             `;
         })
         .join("")}
@@ -96,65 +89,6 @@ async function initMap() {
         });
       });
     });
-
-    // items.forEach( item => {
-    //   const latitude = item.latitude;
-    //   const longitude = item.longitude;
-    //   const postPath = item.postPath;
-    //   const title = item.title;
-    //   const content = item.content;
-    //   const category = item.category;
-    //   const categoryValue = item.categoryValue;
-    //   const price = item.price;
-    //   const timing = item.timing;
-    //   const fullAddress = item.fullAddress;
-    //   const userName = item.user.name;
-    //   const userImage = item.user.image;
-    //   const createdAt = item.createdAt;
-    //   const marker = new google.maps.marker.AdvancedMarkerElement({
-    //     position: { lat: latitude, lng: longitude },
-    //     map,
-    //     title: title,
-    //   });
-
-    //   const contentString = `
-
-    //   <div class="container mt-3">
-    //     <div class="row">
-    //       <div class="col">
-    //         <div class="card mb-4">
-    //           <div class="card-header">
-    //             <span class="category-item d-inline-block text-center" data-category="${categoryValue}">
-    //               ${category}
-    //             </span>
-    //             <strong class="ml-3"><a href="${postPath}" class="text-dark">${title}</a></strong>
-    //           </div>
-    //           <div class="card-body">
-    //             <p class="card-text">${content}</p>
-    //           </div>
-    //           <div class="card-footer text-muted">
-    //             <span class="mr-3">${createdAt}</span>
-    //             <span>${fullAddress}</span>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-      
-    //   `;
-
-    //   const infowindow = new google.maps.InfoWindow({
-    //     content: contentString,
-    //     ariaLabel: title,
-    //   });
-
-    //   marker.addListener("click", () => {
-    //       infowindow.open({
-    //       anchor: marker,
-    //       map,
-    //     })
-    //   });
-    // });
   } catch (error) {
       console.error('投稿画像の取得または処理中にエラーが発生しました:', error);
   }
